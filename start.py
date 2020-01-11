5177ff650a9db32fbbcb40a8a1b2c05f4b6d936b
Jordan BRION <jrd.brion@gmail.com>
2020-01-11 17:39:24 +0100

    start.py: call Rust function via cffi

--- start.py
+++ start.py
@@ -1,8 +1,17 @@
 import sys
 from PySide2.QtWidgets import QApplication
 from ui_frontend.py.mainwindow_impl import MainWindow_Impl
+from cffi import FFI
+ffi = FFI()
+ffi.cdef("""
+    int mul(int);
+""")
+C = ffi.dlopen("/home/jordanbrion/Documents/rust/pacman_gameengine/ui_backend/target/debug/libui_backend.so")
+
+print(C.mul(9))
 
 if __name__ == "__main__":
+
     app = QApplication(sys.argv)
 
     window = MainWindow_Impl()
