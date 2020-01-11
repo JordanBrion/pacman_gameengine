2acb41c140ab80496e18ee093397b2f861598f60
Jordan BRION <jrd.brion@gmail.com>
2020-01-11 16:59:18 +0100

    mainwindow_impl.py: connect slot to button signal

--- ui_frontend/py/mainwindow_impl.py
+++ ui_frontend/py/mainwindow_impl.py
@@ -1,6 +1,6 @@
 import sys
 from PySide2.QtWidgets import QApplication, QMainWindow
-from PySide2.QtCore import QFile
+from PySide2.QtCore import Qt, Slot, QFile
 from .mainwindow_ui import Ui_MainWindow
 
 class MainWindow_Impl(QMainWindow):
@@ -8,3 +8,15 @@ class MainWindow_Impl(QMainWindow):
         super(MainWindow_Impl, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.connect_slots()
+
+    def connect_slots(self):
+        self.ui.pushButton.clicked.connect(self.cb_push_button_clicked)
+
+    def create_new_project(self):
+        print("Creating new project...");
+
+    @Slot()
+    def cb_push_button_clicked(self):
+        self.create_new_project();
+        
