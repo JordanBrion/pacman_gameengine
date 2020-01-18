0c44d645e01c6ef29becf219dc0ce723e295a473
Jordan BRION <jrd.brion@gmail.com>
2020-01-18 12:42:50 +0100

    lib.rs: define function callable from C++

--- xxffi/src/lib.rs
+++ xxffi/src/lib.rs
@@ -1,7 +1,12 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#[no_mangle]
+pub unsafe extern "C" fn request_create() {
+    println!("Hello from Rust");
 }
+
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn it_works() {
+//         assert_eq!(2 + 2, 4);
+//     }
+// }
