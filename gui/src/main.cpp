257f9e4f14d8a9a436edbe6a673ad128031c2816
Jordan BRION <jrd.brion@gmail.com>
2020-01-20 20:24:06 +0100

    main.cpp: create MainWindow instance

--- gui/src/main.cpp
+++ gui/src/main.cpp
@@ -1,8 +1,14 @@
-#include <iostream>
+#include "mainwindow.h"
+
+#include <QApplication>
 
 #include <xxffi/xxffi.h>
 
-int main() {
-    std::cout << "hello" << std::endl;
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
     request_create();
+    return a.exec();
 }
