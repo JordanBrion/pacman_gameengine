f120a8ef97ac21913423543f1cc637eac7e3ab00
Jordan BRION <jrd.brion@gmail.com>
2020-01-24 17:19:16 +0100

    lib.rs: print serialized Rust struct

--- xxffi/src/lib.rs
+++ xxffi/src/lib.rs
@@ -1,6 +1,26 @@
+extern crate serde;
+extern crate serde_json;
+
+use serde::{Deserialize, Serialize};
+
 #[no_mangle]
-pub unsafe extern "C" fn request_create() {
+pub unsafe extern "C" fn hello_from_rust() {
     println!("Hello from Rust");
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Point {
+    x: i32,
+    y: i32,
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn serialize_rust_struct() {
+    let p1 = Point { x: 100, y: 200 };
+    let serialized: std::string::String = serde_json::to_string(&p1).unwrap();
+    println!("aaaaa {}", serialized);
+    // let c_serialized = serialized.
+    // serialized;
 }
 
 // #[cfg(test)]
