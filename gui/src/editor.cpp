beb12b650087a32f32da9eb7523cf65720977b69
Jordan BRION <jrd.brion@gmail.com>
2020-01-26 11:49:16 +0100

    cbindgen.toml: add include guard when compiling xxffi

--- gui/src/editor.cpp
+++ gui/src/editor.cpp
@@ -13,13 +13,13 @@ Editor::Editor(int argc, char* argv[])
     : QApplication(argc, argv)
     , m_mainWindow()
 {
-    hello_from_rust();
-    const auto serialized = serialize_rust_struct();
+    xxffi::hello_from_rust();
+    const auto serialized = xxffi::serialize_rust_struct();
     qDebug() << "serialized !!" << serialized;
     delete[] serialized;
-    const auto point = get_rust_instance();
+    const auto point = xxffi::get_rust_instance();
     qDebug() << point.x;
     qDebug() << point.y;
-    my_point_print_hello(&point);
+    xxffi::my_point_print_hello(&point);
     m_mainWindow.show();
 }
